6f8c5716e050680998d7be43fd0e8f44c03c993b
Oliver Bing <ailurus842@gmail.com>
2023-09-22 15:34:22 +0200

    i think this should go here

--- interfaces.ts
+++ interfaces.ts
@@ -33,27 +33,31 @@ interface HouseWithID {
 }
 
 function findHouses(houses: string): HouseWithID[];
+
 function findHouses(
     houses: string,
     filter: (house: House) => boolean
 ): HouseWithID[];
+
 function findHouses(houses: House[]): HouseWithID[];
+
 function findHouses(
     houses: House[],
     filter: (house: House) => boolean
 ): HouseWithID[];
 
-function findHouses(arg1: string | House[]): HouseWithID[] {
-    if (typeof arg1 === "string") {
-        const parsedHouses = JSON.parse(arg1)
-        const housesWithID = parsedHouses.map((house: House): HouseWithID => ({
-            ...house,
-            id: uuidv4()
-        }))
 
-        return housesWithID
-    }
-    const housesWithID = arg1.map((house: House): HouseWithID => ({
+function findHouses(
+    arg1: string | House[],
+    arg2?: (house: House) => boolean
+): HouseWithID[] {
+    let parsedHouses: House[]
+
+    typeof arg1 === "string" ? parsedHouses = JSON.parse(arg1) : parsedHouses = arg1
+    
+    arg2 ? parsedHouses = parsedHouses.filter(house => arg2(house)) : null
+
+    const housesWithID = parsedHouses.map(house => ({
         ...house,
         id: uuidv4()
     }))
@@ -61,8 +65,10 @@ function findHouses(arg1: string | House[]): HouseWithID[] {
     return housesWithID
 }
 
-console.log(findHouses(JSON.stringify(houses)))
+console.log(1, findHouses(JSON.stringify(houses)))
 
-console.log(findHouses(JSON.stringify(houses), ({ name }) => name === "Atreides"))
+console.log(2, findHouses(JSON.stringify(houses), ({ name }) => name === "Corrino"))
 
-console.log(findHouses(houses))
+console.log(3, findHouses(houses))
+
+console.log(4, findHouses(houses, ({ name }) => name === "Harkonnen"))
