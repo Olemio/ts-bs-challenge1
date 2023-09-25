d445247fc205d0c818908c01f3c6dc3dfabd83c2
Oliver Bing <ailurus842@gmail.com>
2023-09-25 17:22:33 +0200

    move house data to json file

--- interfaces.ts
+++ interfaces.ts
@@ -1,47 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
-
-const houses: House[] = [
-    {
-        "name": "Atreides",
-        "planets": "Calladan"
-    },
-    {
-        "name": "Corrino",
-        "planets": [
-            "Kaitan",
-            "Salusa Secundus"
-        ]
-    },
-    {
-        "name": "Harkonnen",
-        "planets": [
-            "Giedi Prime",
-            "Arrakis"
-        ]
-    }
-]
+import houses from "./houses.json"
 
 interface House {
     name: string
     planets: string | string[]
 }
-
-interface HouseWithID {
-    name: string
-    planets: string | string[]
+interface HouseWithID extends House {
     id: string
 }
-
-function findHouses(houses: string): HouseWithID[];
-function findHouses(
-    houses: string,
-    filter: (house: House) => boolean
-): HouseWithID[];
-function findHouses(houses: House[]): HouseWithID[];
-function findHouses(
-    houses: House[],
-    filter: (house: House) => boolean
-): HouseWithID[];
 
 function findHouses(
     arg1: string | House[],
